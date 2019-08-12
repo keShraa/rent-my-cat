d99df975592e5dc454325697d7b0b5600cae2156
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-12 16:40:53 +0200

    cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :cats, only: [:index]
-
-
-=======
   devise_for :users
->>>>>>> 77de55cd89576262d32a9f15c8f67f7a940619f0
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
