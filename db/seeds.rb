23826f311e9018ef11b88f760c581ca621a7cec0
Jonathan Wagner <jonathan.wagner88@gmail.com>
2019-08-12 18:06:06 +0200

    seeds modified and uploader

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,17 @@ puts "Cleaning DB"
 
 Cat.destroy_all
 
+user = User.new
+
 100.times do
   url = "https://source.unsplash.com/collection/139386/1000x1000/?sig="
   cat = Cat.new(
     name: Faker::Creature::Cat.name,
     breed: Faker::Creature::Cat.breed,
-    address: ["Bordeaux", "Paris", "Lille", "Marseille", "Lyon", "Nice", "Cannes", "Montpellier", "Dijon", "Metz", "Strasbourg", "Toulouse", "Brest", "Avignon", "Bayonne"].shuffle.first
+    address: ["Bordeaux", "Paris", "Lille", "Marseille", "Lyon", "Nice", "Cannes", "Montpellier", "Dijon", "Metz", "Strasbourg", "Toulouse", "Brest", "Avignon", "Bayonne"].shuffle.first,
     age: rand(1..22),
     price_per_day: rand(5..172),
+    user_id: 1,
     description: ["He is very friendly, loves playing and is very kind with children", "She sleeps all day and eats a lot. She loves going outside and eat the grass or play with insects" , "He loves watching TV and being pet. Excellent hunter, he might bring you mouses as a gift and will be very proud of it. He can easily escape so preferences for people whose house has fences"].shuffle.first
     )
   cat.remote_photo_url = url
