ae0d3dc8b6dd82dcd73add7126ae742bab8d9d36
Jonathan Wagner <jonathan.wagner88@gmail.com>
2019-08-12 17:42:53 +0200

    added uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_135258) do
+ActiveRecord::Schema.define(version: 2019_08_12_153843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,13 @@ ActiveRecord::Schema.define(version: 2019_08_12_135258) do
     t.string "name"
     t.string "description"
     t.string "address"
-    t.string "photo"
     t.string "breed"
     t.integer "age"
     t.integer "price_per_day"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ puts "Cleaning DB"
 
 Cat.destroy_all
 
-50.times do
+100.times do
   url = "https://source.unsplash.com/collection/139386/1000x1000/?sig="
   cat = Cat.new(
     name: Faker::Creature::Cat.name,
     breed: Faker::Creature::Cat.breed,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    address: ["Bordeaux", "Paris", "Lille", "Marseille", "Lyon", "Nice", "Cannes", "Montpellier", "Dijon", "Metz", "Strasbourg", "Toulouse", "Brest", "Avignon", "Bayonne"].shuffle.first
     age: rand(1..22),
     price_per_day: rand(5..172),
     description: ["He is very friendly, loves playing and is very kind with children", "She sleeps all day and eats a lot. She loves going outside and eat the grass or play with insects" , "He loves watching TV and being pet. Excellent hunter, he might bring you mouses as a gift and will be very proud of it. He can easily escape so preferences for people whose house has fences"].shuffle.first
