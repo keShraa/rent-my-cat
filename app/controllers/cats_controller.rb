8dd14c362aa41a47496856a4508485c12b431a70
julien legay <legayj@gmail.com>
2019-08-13 11:49:47 +0200

    authorize-cat#show-without-signin

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,5 @@
 class CatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_cat, only: [:show, :edit, :update, :destroy]
 
   def index
