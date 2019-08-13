00a80d0163ba5e5601ccb639878b505c78ef6e80
Jonathan Wagner <jonathan.wagner88@gmail.com>
2019-08-13 10:40:03 +0200

    Pictures cats implemented, users model column added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :age, inclusion: { in: 18..100 }
+  # validates :age, inclusion: { }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :cats
   has_many :booking, dependent: :destroy
-
 end
