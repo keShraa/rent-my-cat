9286b80ec8275f75720175d55d1a101a4fad7b43
Florian Kogon <F.Kogon@gmail.com>
2019-08-13 11:01:40 +0200

    less cats and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Cleaning DB..."
+Cat.destroy_all
+User.destroy_all
+Booking.destroy_all
 
-users = []
 
-25.times do
+10.times do
   user = User.new(email: Faker::Internet.email, password: "password")
   user.save!
 end
 
-Cat.destroy_all
 
-50.times do
+20.times do
   url = "https://source.unsplash.com/collection/139386/1000x1000/?sig="
   cat = Cat.new(
     name: Faker::Creature::Cat.name,
