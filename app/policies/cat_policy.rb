1ab67cd22ed8f7da3567a80d0f799d86511421df
Florian Kogon <F.Kogon@gmail.com>
2019-08-13 15:10:11 +0200

    add all method to cat policy

--- app/policies/cat_policy.rb
+++ app/policies/cat_policy.rb
@@ -5,6 +5,18 @@ class CatPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
   def update?
     record.user == user
   end
