7f5d0885f4add8dddf22c7cd7e55ea9e6403a18c
Florian Kogon <F.Kogon@gmail.com>
2019-08-14 16:13:42 +0200

    cleaning mess done in cat controller

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -10,7 +10,6 @@ class Cat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-
   has_many :booking, dependent: :destroy
   belongs_to :user
   mount_uploader :photo, PhotoUploader
