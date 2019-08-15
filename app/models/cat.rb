53fc518c985914e2a5583b138fcd66d8e6a1976b
Florian Kogon <F.Kogon@gmail.com>
2019-08-15 14:21:49 +0200

    range plugin add

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -10,7 +10,6 @@ class Cat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-
   has_many :booking, dependent: :destroy
   belongs_to :user
   enum status: %i[available unavailable]
