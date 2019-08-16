d4562a3248dcba2db903d6dc421d7a60a0c480e8
julien legay <legayj@gmail.com>
2019-08-16 12:28:38 +0200

    add review into cat#show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,23 @@
 class ReviewsController < ApplicationController
-  def new
-    @cat = Cat.find(params[:cat_id])
-    @review = Review.new
-    authorize @review
-  end
+  skip_before_action :verify_authenticity_token
 
   def create
     @cat = Cat.find(params[:cat_id])
     @review = Review.new(params_review)
+    @review.cat = @cat
     @review.user_id = current_user.id
     authorize @review
 
-    @review.cat = @cat
     if @review.save
-      redirect_to cat_path(@cat)
+      respond_to do |format|
+        format.html { redirect_to cat_path(@cat) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render 'cats/show' }
+        format.js  # <-- idem
+      end
     end
   end
 
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module RentMyCat
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.initialize_on_precompile = false
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
 
